298f4c4388196e1b7edac864618c482f2bc34fba
Jon <jonhasacat8@gmail.com>
2026-02-24 12:08:15 -0800

    BackgroundJobInline is completely unnecessary as the cobalt-background binary is also accessible directly.

--- src/cli/background_runner.go
+++ src/cli/background_runner.go
@@ -2,8 +2,6 @@ package cli
 
 import (
 	"errors"
-	"fmt"
-	"log"
 	"vatusa-cobalt/background"
 )
 
@@ -21,28 +19,6 @@ func RunBackgroundJob(args []string) error {
 	err := job.Run()
 	if err != nil {
 		return err
-	}
-	return nil
-}
-
-func RunBackgroundJobInline(args []string) error {
-	if len(args) < 1 {
-		return errors.New("must provide a job name")
-	}
-	jobName := args[0]
-	jobArgs := []string{}
-	if len(args) > 1 {
-		jobArgs = args[1:]
-	}
-
-	jobFunc, ok := background.JobDefinitions[jobName]
-	if !ok {
-		return errors.New(fmt.Sprintf("job '%s' not found", jobName))
-	}
-
-	err := jobFunc(jobArgs)
-	if err != nil {
-		log.Fatal(err)
 	}
 	return nil
 }
--- src/cli/entrypoint.go
+++ src/cli/entrypoint.go
@@ -3,6 +3,5 @@ package cli
 type CommandFunc func(args []string) error
 
 var Commands = map[string]CommandFunc{
-	"run":        RunBackgroundJob,
-	"run_inline": RunBackgroundJobInline,
+	"run": RunBackgroundJob,
 }
