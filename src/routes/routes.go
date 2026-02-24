f974f3b3bc115b6e14c7a8168e9e142ea2d57a49
Jon <jonhasacat8@gmail.com>
2026-02-23 21:16:50 -0800

    Remove deprecated /api endpoints.

--- src/routes/routes.go
+++ src/routes/routes.go
@@ -48,13 +48,6 @@ func SetupRoutes(e *echo.Echo) {
 	event.POST("/:id", handler.UpdateEvent)
 	event.DELETE("/:id", handler.DeleteEvent)
 
-	api := e.Group("/api")
-	api.GET("/token/:cid", handler.GetGenerateUserToken)
-	api.POST("/roles/legacy_sync", handler.LegacySyncRoles)
-	api.GET("/news/:id", handler.GetPost)
-	api.GET("/news/latest/:count", handler.GetLastPosts)
-	api.GET("/news/page/:page", handler.GetNewsPage)
-
 	login := e.Group("/login")
 	login.GET("", handler.GetLogin)
 	login.GET("/connect", handler.Connect)
